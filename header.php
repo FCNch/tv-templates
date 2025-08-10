3f452867c65a5cafa575f457846d14cdbbab864a
FCN <71300684+FCNch@users.noreply.github.com>
2025-08-11 00:42:26 +0600

    Update header.php

--- header.php
+++ header.php
@@ -26,5 +26,18 @@
     }
   </style>
 </head>
-<body class="bg-gray-950 text-white font-sans h-screen overflow-hidden">
-  <div class="flex flex-col md:flex-row h-full max-h-screen">
+<body class="bg-gray-900 text-white">
+    <div class="flex flex-col md:flex-row h-full max-h-screen">
+        <!-- Header -->
+    <header class="bg-gray-800 p-4 shadow-md">
+      <div class="container mx-auto flex justify-between items-center">
+        <h1 class="text-xl font-bold">BKD LIVE</h1>
+        <nav class="space-x-4">
+          <a href="http://web.ssrbinternet.com/" class="text-teal-300 hover:underline">HOME</a>
+          <a href="https://cdn.fcnchbd.xyz/" class="text-teal-300 hover:underline">EFN IPTV</a>
+          <a href="https://iptv.fcnchbd.xyz/" class="text-teal-300 hover:underline">FIPTV</a>
+        </nav>
+      </div>
+    </header>
+
+  
